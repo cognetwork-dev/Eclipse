2faacbc5589d1898db4e30a39c594d33066517c3
Nebelung <nebelung@mailfence.com>
2022-12-28 04:05:53 +0000

    a

--- eclipse/rewrite/html.js
+++ eclipse/rewrite/html.js
@@ -1,4 +1,47 @@
 import { parse, serialize } from "parse5";
+import { url } from "./index.js"
+
+function html(code, requestURL, prefix) {
+    var dom = parse(code)
+    for (let node in dom.childNodes) {
+        dom.childNodes[node] = addNode(dom.childNodes[node]);
+    }
+    rewriteNodes(requestURL, prefix)
+    return serialize(dom)
+}
+
+var elements = []
+
+function addNode(node) {
+    elements.push(node)
+    if (node.childNodes) {
+        for (let childNode in node.childNodes) {
+            node.childNodes[childNode] = addNode(node.childNodes[childNode]);
+        }
+    }
+    return node;
+}
+
+function rewriteNodes(requestURL, prefix) {
+for (var config in HTML_REWRITER) {
+    if (HTML_REWRITER[config].action == "rewrite") {
+      HTML_REWRITER[config].attrs.forEach((attr) => {
+        var hasAttributes = elements.filter(elem => elem.attrs)
+        for (let item in hasAttributes) {
+          for (let attribute in hasAttributes[item].attrs) {
+            if (hasAttributes[item].attrs[attribute].name == attr) {
+              hasAttributes[item].attrs.push({
+                name: "eclipse-" + attr,
+                value: hasAttributes[item].attrs[attribute].value
+              });
+              hasAttributes[item].attrs[attribute].value = url(requestURL, hasAttributes[item].attrs[attribute].value, prefix)
+            }
+          }
+        }
+      })
+    }
+}
+}
 
 var HTML_REWRITER = [
   {
@@ -34,45 +77,5 @@ var HTML_REWRITER = [
     action: "inject"
   }
 ]
-
-function html(code, requestURL, prefix) {
-    var dom = parse(code)
-    for (let node in dom.childNodes) {
-        dom.childNodes[node] = addNode(dom.childNodes[node]);
-    }
-    rewriteNodes()
-    return serialize(dom)
-}
-
-var elements = []
-
-function addNode(node) {
-    elements.push(node)
-    if (node.childNodes) {
-        for (let childNode in node.childNodes) {
-            node.childNodes[childNode] = addNode(node.childNodes[childNode]);
-        }
-    }
-    return node;
-}
-
-function rewriteNodes() {
-console.log(elements)
-console.log(elements.filter(item => item.attrs !== null))
-  /*
-for (var config in HTML_REWRITER) {
-    if (HTML_REWRITER[config].action == "rewrite") {
-      HTML_REWRITER[config].attrs.forEach((attr) => {
-        var allelems = elements.filter(elem => elem.hasAttribute(attr))
-        for (var aelem in allelems) {
-          if (allelems[aelem].hasAttribute(attr)) {
-            allelems[aelem].setAttribute(attr, "test")
-          }
-        }
-      })
-    }
-}
-*/
-}
 
 export { html as default };
